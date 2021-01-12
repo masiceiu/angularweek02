72b15a397e289179813a095f2d4c6912b17d00ae
Abdus.Sobhan <masiceiu@gmail.com>
2021-01-12 09:35:39 +0600

    AngularWeek2 Initial Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularweek02'`, () => {
+  it(`should have as title 'angularweek01assignment'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularweek02');
+    expect(app.title).toEqual('angularweek01assignment');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angularweek02 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angularweek01assignment app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HeaderComponent } from './common/component/header/header.component';
+import { FooterComponent } from './common/component/footer/footer.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { CardComponent } from './shared/component/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
